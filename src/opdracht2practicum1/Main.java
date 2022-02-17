bacc4ded119be1026f48e5ed659f8fd590e63dff
davidcoffa <david.coffa@student.hu.nl>
2022-02-17 15:25:02 +0100

    Practicum 1

---
+++ src/opdracht2practicum1/Main.java
@@ -0,0 +1,11 @@
+package opdracht2practicum1;
+
+public class Main {
+    public static void main(String[] args){
+        int count = 1;
+        while(count < 11){
+            System.out.println(count);
+          count ++;
+        }
+    }
+}
---
+++ src/opdracht3practicum1/Main.java
@@ -0,0 +1,10 @@
+package opdracht3practicum1;
+
+public class Main {
+    public static void main(String[] args){
+        for(int i=1; i < 11; i ++){
+            System.out.println(Math.random());
+        }
+
+    }
+}
